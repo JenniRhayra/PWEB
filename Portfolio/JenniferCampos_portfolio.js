c293d3021436387d766d2669dae19d3ef7eb260e
Jennifer Campos <jenniferrhayra@gmail.com>
2023-05-28 11:34:40 -0300

    20230528_alteradopatanovapasta

--- Portfolio/JenniferCampos_portfolio.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const btnMobile = document.querySelector(".btn-mobile");
-    const menuMobile = document.querySelector(".menu-mobile");
-    
-    btnMobile.addEventListener("click", function(){
-        menuMobile.classList.toggle("show");
-    });
-});
